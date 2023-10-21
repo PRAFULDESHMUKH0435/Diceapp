10d1e5bcc88c45e1cc88e77d927a1c02895f1d25
praful Deshmukh <prafuldeshmukh068@gmail.com>
2023-10-21 11:43:07 -0700

    final changes before generating app

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
